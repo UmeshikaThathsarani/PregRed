74d0952eadf6857c5e026659f9913de3e2bd7484
UmeshikaMahaliyadda <umeshika.2018859@iit.ac.lk>
2021-03-24 15:22:44 +0530

    test2

--- pregred-gui/src/app/pages/home/Home.js
+++ pregred-gui/src/app/pages/home/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './Home.css';
 
 function Home() {
-  return <h1> PregRed App is Running - Home </h1>;
+  return <h1> PregRed App is Running - Home1 </h1>;
 }
 
 export default Home;
