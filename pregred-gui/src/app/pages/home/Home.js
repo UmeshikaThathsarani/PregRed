949b1db85bfb29b3e47278f57175b26ebbadc9d1
UmeshikaMahaliyadda <umeshika.2018859@iit.ac.lk>
2021-03-25 18:01:52 +0530

    corrected intendation in buildspec.yml

--- pregred-gui/src/app/pages/home/Home.js
+++ pregred-gui/src/app/pages/home/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './Home.css';
 
 function Home() {
-  return <h1> PregRed App is Running - Home1 </h1>;
+  return <h1> PregRed App is Running - Home2 </h1>;
 }
 
 export default Home;
