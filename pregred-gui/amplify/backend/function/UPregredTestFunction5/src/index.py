618ac5781b4221d1ce7477dfc00455bc4237462e
UmeshikaMahaliyadda <umeshika.2018859@iit.ac.lk>
2021-04-14 11:27:20 +0530

    set Content-Type

--- pregred-gui/amplify/backend/function/UPregredTestFunction5/src/index.py
+++ pregred-gui/amplify/backend/function/UPregredTestFunction5/src/index.py
@@ -7,6 +7,7 @@ def handler(event, context):
   return {
       'statusCode': 200,
       'headers': {
+          'Content-Type': 'application/json', 
           'Access-Control-Allow-Headers': '*',
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
