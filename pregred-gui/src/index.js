1abf1871f095a1db24285ce2c696f750dcab6ffa
Chiran2018814 <chiran.2018814@iit.ac.lk>
2021-04-14 13:41:16 +0530

    add empty line

--- pregred-gui/src/index.js
+++ pregred-gui/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom';
 import Amplify from 'aws-amplify';
 import App from './app/App';
 
 import './index.css';
 
-
 import config from './aws-exports'
+
 Amplify.configure(config)
 
 ReactDOM.render(
