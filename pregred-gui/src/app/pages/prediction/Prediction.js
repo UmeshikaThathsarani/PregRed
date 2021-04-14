7dafac757aa6a560c18cf6fe43f4939a2ad9999b
Chiran2018814 <chiran.2018814@iit.ac.lk>
2021-04-14 06:55:10 +0530

    test

--- pregred-gui/src/app/pages/prediction/Prediction.js
+++ pregred-gui/src/app/pages/prediction/Prediction.js
@@ -1,10 +1,10 @@
 import React from 'react';
 // import Popup from "reactjs-popup";
 import './Prediction.css';
-import Amplify from "aws-amplify";
-import { API } from "aws-amplify";
-import awsExports from "./aws-exports";
-Amplify.configure(awsExports);
+// import Amplify from "aws-amplify";
+// import { API } from "aws-amplify";
+// import awsExports from "./aws-exports";
+// Amplify.configure(awsExports);
 
 function Alert() {
   alert("Hello")
