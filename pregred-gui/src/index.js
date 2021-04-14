bacc73e6136964e17102f6fbace2ec8ce12b08b0
UmeshikaMahaliyadda <umeshika.2018859@iit.ac.lk>
2021-04-14 18:58:27 +0530

    removed Amplify configurations

--- pregred-gui/src/index.js
+++ pregred-gui/src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Amplify from 'aws-amplify';
-import config from './aws-exports';
 
 import App from './app/App';
 
 import './index.css';
-
-Amplify.configure(config);
 
 ReactDOM.render(
   <React.StrictMode>
