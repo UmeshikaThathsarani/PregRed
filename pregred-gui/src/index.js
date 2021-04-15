6d443fa7055e9364404578cfeaf3006ae3c1cfee
UmeshikaMahaliyadda <umeshika.2018859@iit.ac.lk>
2021-04-15 21:27:06 +0530

    added aws-exports

--- pregred-gui/src/index.js
+++ pregred-gui/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Amplify from "aws-amplify";
+import Amplify, {API} from "aws-amplify";
+import config from "./aws-exports";
 
 import ReactDOM from 'react-dom';
 
@@ -7,17 +8,18 @@ import App from './app/App';
 
 import './index.css';
 
-Amplify.configure({
-  API: {
-    endpoints: [
-        {
-            name: "UTest7API",
-            endpoint: "https://t6e2noaphk.execute-api.ap-southeast-1.amazonaws.com/dev",
-            region: "ap-southeast-1"
-        }
-    ]
-}
-});
+Amplify.configure(config);
+// Amplify.configure({
+//   API: {
+//     endpoints: [
+//         {
+//             name: "UTest7API",
+//             endpoint: "https://t6e2noaphk.execute-api.ap-southeast-1.amazonaws.com/dev",
+//             region: "ap-southeast-1"
+//         }
+//     ]
+// }
+// });
 
 ReactDOM.render(
   <React.StrictMode>
