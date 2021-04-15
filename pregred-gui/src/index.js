be3435ad43cbbcb16febbf60335ae68aed19635d
UmeshikaMahaliyadda <umeshika.2018859@iit.ac.lk>
2021-04-15 11:59:15 +0530

    added amplify configurations

--- pregred-gui/src/index.js
+++ pregred-gui/src/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
 import ReactDOM from 'react-dom';
 
 import App from './app/App';
