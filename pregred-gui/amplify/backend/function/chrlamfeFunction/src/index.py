561c584d8a2b1eb49f2cda7edab06a5a41c09d46
Chiran2018814 <chiran.2018814@iit.ac.lk>
2021-04-17 10:34:16 +0530

    e3dit lambda

--- pregred-gui/amplify/backend/function/chrlamfeFunction/src/index.py
+++ pregred-gui/amplify/backend/function/chrlamfeFunction/src/index.py
@@ -1,15 +1,29 @@
 import json
 
-def handler(event, context):
-  print('received event:')
-  print(event)
+# def handler(event, context):
+#   print('received event:')
+#   print(event)
   
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
-  }
+#   return {
+#       'statusCode': 200,
+#       'headers': {
+#           'Access-Control-Allow-Headers': '*',
+#           'Access-Control-Allow-Origin': '*',
+#           'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+#       },
+#       'body': json.dumps('Hello from your new Amplify Python lambda!')
+#   }
+
+def lambda_handler (event, context):
+    transactionID = event['queryStringParameters']['transactionID']
+
+    transactionResponse['transactionID'] = transactionID
+    transactionResponse['message'] = "Hello from Chiran"
+
+    responseObject = {}
+    responseObject['statusCode'] = 200
+    responseObject['headers'] {}
+    responseObject['headers']['Content-Type'] = 'application/json'
+    responseObject['body'] = json.dumps(transactionResponse)
+
+    return responseObject
