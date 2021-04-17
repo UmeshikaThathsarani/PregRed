915b15c6b5176b0637aa4d281697cb6e3ba20eeb
Chiran2018814 <chiran.2018814@iit.ac.lk>
2021-04-17 14:30:07 +0530

    a

--- pregred-gui/src/app/pages/prediction/Prediction.js
+++ pregred-gui/src/app/pages/prediction/Prediction.js
@@ -74,7 +74,7 @@ import { Button } from 'react-bootstrap';
 import Amplify from "aws-amplify";
 import { API } from 'aws-amplify';
 
-import awsExports from "./aws-exports";
+import awsExports from "aws-exports";
 Amplify.configure(awsExports);
 
 async function inputs() {
