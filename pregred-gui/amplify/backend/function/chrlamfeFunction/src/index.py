1e58df453b9151bf06130a17c18401c050d41970
Chiran2018814 <chiran.2018814@iit.ac.lk>
2021-04-17 12:20:51 +0530

    change

--- pregred-gui/amplify/backend/function/chrlamfeFunction/src/index.py
+++ pregred-gui/amplify/backend/function/chrlamfeFunction/src/index.py
@@ -1,5 +1,14 @@
 import json
 
+import pandas as pd
+import tensorflow as tf
+from tensorflow import keras
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+import warnings;
+import pickle;
+
 # def handler(event, context):
 #   print('received event:')
 #   print(event)
@@ -27,3 +36,39 @@ def lambda_handler (event, context):
     responseObject['body'] = json.dumps(transactionResponse)
 
     return responseObject
+
+
+
+df = pd.read_csv(r'E:\3 rd year semester 1--10-10-2020\SDGP ---11-11-2020\Implementation----07-03-2021\Weka_PregRed_binary_Dataset.csv')
+
+properties = list(df.columns.values)
+properties.remove('Prediction')
+X = df[properties]
+y = df['Prediction']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(13,)),
+    keras.layers.Dense(16, activation=tf.nn.relu),
+	keras.layers.Dense(16, activation=tf.nn.relu),
+    keras.layers.Dense(1, activation=tf.nn.sigmoid),
+])
+
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(X_train, y_train, epochs=39, batch_size=1)
+
+
+
+test_loss, test_acc = model.evaluate(X_test, y_test)
+print('Test accuracy:', test_acc)
+
+a= np.array([[0.89899, 0.648649, 0, 0.414141, 0.5, 0.439344, 0, 0.333333, 0,0, 0, 0, 0],
+             [0.444444, 0.378378, 0, 0.494949,0.3125, 0.272131, 0, 0.166667, 0, 1, 1, 0, 0]])
+print(model.predict(a))
+
+pickle.dump(model,open('model.pkl','wb'))
+model= pickle.load(open('model.pkl','rb'))
