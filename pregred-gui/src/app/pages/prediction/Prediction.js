18404a2280fc5b0d6d102dfacfd51de19eaae427
Chiran2018814 <chiran.2018814@iit.ac.lk>
2021-04-20 21:06:28 +0530

    vhng

--- pregred-gui/src/app/pages/prediction/Prediction.js
+++ pregred-gui/src/app/pages/prediction/Prediction.js
@@ -36,7 +36,7 @@ import axios from 'axios';
       console.log(dataSet)
 
       axios
-      .post('https://saue4gu7v4.execute-api.ap-southeast-1.amazonaws.com/dev/clrformdt',dataSet)
+      .post('https://saue4gu7v4.execute-api.ap-southeast-1.amazonaws.com/dev/clrformdt/post',dataSet)
       .then(res => console.log(res.data));
     }
 
